61c45cd3df8c71d7ea1d3b6f326d48b2f51d762d
KateStearns <katestearns@mac.com>
2015-09-22 15:56:29 -0700

    survey returns correct recommended size

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
 $(document).ready(function() {
 
+  var curCup;
   var currentSize = [];
   var $questions = $('.surveyQ');
-  function grabQuestion(num) {
-    $($questions).hide();
-    $($questions[num]).fadeIn();
-    return num;
-  }
+  var cupSizes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
   var inchesToBandMeasurement = {
-
     '27-29"': 32,
     '29-32"': 34,
     '33-34"': 36,
@@ -19,68 +15,166 @@ $(document).ready(function() {
     '48-49"': 48
   }
 
-  var cupSizes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+ // hide questions and show one at a time;
+  function grabQuestion(num) {
+    $($questions).hide();
+    $($questions[num]).fadeIn();
+    return num;
+  }
 
+  //current size
   $('#quest1').on('click', 'button', function(e) {
     e.preventDefault();
     var bandSize = $('#bandSize option:selected').val();
     var cupSize = $('#cupSize option:selected').val();
     currentSize.push(bandSize, cupSize);
     console.log(currentSize);
-    // db.Size.create({
-    //   currentBandSize: bandSize,
-    //   currentCupSize: cupSize
-    // });
     grabQuestion(1);
   })
+  // ribcage measurement
   $('#quest2').on('click', 'button', function(e) {
     e.preventDefault();
     var question2Ans = $('#quest2 option:selected').val();
-    console.log(inchesToBandMeasurement[question2Ans]);
     currentSize.push(inchesToBandMeasurement[question2Ans]);
+    console.log(currentSize);
     grabQuestion(2);
   })
+  // bra band placement
   $('#quest3').on('click', 'button', function(e) {
     e.preventDefault();
     var question3Ans = $('#question3 input:checked').val();
-    console.log(question3Ans);
     if(question3Ans === 'High') {
-      currentSize.push(eval(currentSize[0] - '2'));
+      currentSize.push(eval(currentSize[0] - 2));
     }
+    console.log(currentSize);
     grabQuestion(3);
   })
+  // wire pain in wire
   $('#quest4').on('click', 'button', function(e) {
     e.preventDefault();
+    curCup = cupSizes.indexOf(currentSize[1]);
     var question4Ans = $('#question4 input:checked').val();
-    console.log(question4Ans);
+    if (question4Ans === 'chestPain') {
+      currentSize.push(cupSizes[curCup - 1]);
+    } else if (question4Ans === 'cutInSide') {
+      currentSize.push(cupSizes[curCup + 1]);
+    } else {
+      currentSize.push(cupSizes[curCup]);
+    }
+    console.log(currentSize);
     grabQuestion(4);
   })
+  //cup preferences
   $('#quest5').on('click', 'button', function(e) {
     e.preventDefault();
     var question5Ans = $('#question5 input:checked').val();
-    console.log(question5Ans);
+    console.log(currentSize);
     grabQuestion(5);
   })
+  // wire flat on chest
   $('#quest6').on('click', 'button', function(e) {
     e.preventDefault();
     var question6Ans = $('#question6 input:checked').val();
-    console.log(question6Ans);
+    if (question6Ans === 'wireFlatNo') {
+      curCup = cupSizes.indexOf(currentSize[1]);
+      currentSize.push(cupSizes[curCup + 1]);
+    } else {
+      currentSize.push(currentSize[1]);
+    }
+    console.log(currentSize);
     grabQuestion(6);
   })
+  // cups runeth over
   $('#quest7').on('click', 'button', function(e) {
     e.preventDefault();
     var question7Ans = $('#question7 input:checked').val();
-    console.log(question7Ans);
+    curCup = cupSizes.indexOf(currentSize[1]);
+    if ( question7Ans === 'cupTopGap' ) {
+      currentSize.push(cupSizes[curCup - 1]);
+    } else if ( question7Ans === 'cupDoubleBubble') {
+      currentSize.push(cupSizes[curCup + 1]);
+    } else if ( question7Ans === 'cupBtmGap') {
+      var quest7Band = currentSize[0] - 2;
+      currentSize.push(quest7Band);
+    } else {
+      currentSize.push(currentSize[1]);
+    }
+    console.log(currentSize);
     grabQuestion(7);
   })
+  // brand preferences
   $('#quest8').on('click', 'button', function(e) {
     e.preventDefault();
     var question8Ans = $('#quest8 input:checked').val();
     console.log(question8Ans);
-    grabQuestion(8);
+    giveRecSize(recommendedBand(), recommendedCup());
   })
   grabQuestion(0);
 
+  // get Band recommendation via average of answers
+  function recommendedBand() {
+    var recBand, sum, err= 'error';
+    var givenBand = parseInt(currentSize[0]);
+    console.log(givenBand);
+    if (typeof currentSize[6] !== 'string') {
+      avg = (givenBand + currentSize[2] + currentSize[3] + currentSize[6]) / 4;
+    } else if (typeof currentSize[6] === 'string') {
+      avg = (givenBand + currentSize[2] + currentSize[3]) / 3;
+    } else {
+      console.log('Error')
+    }
+    recBand = Math.floor(avg);
+    if ( recBand % 2 !== 0 ) {
+      recBand = recBand - 1;
+    }
+    console.log('returned band is ', recBand);
+    return recBand || err;
+  }
 
+  // get Cup Size via frequency
+  function recommendedCup() {
+    var givenCup = currentSize[1];
+    var cups = [];
+    var cupA = [], cupB = [];
+    var recCup;
+    cups.push(currentSize[1, 4, 5]);
+    if ( typeof currentSize[6] === 'string' ) {
+      cups.push(currentSize[6]);
+    }
+    cups.sort();
+    for( var i = 0; i < cups.length; i++ ) {
+      if (cups[i] === givenCup) {
+        cupA.push(cups[i]);
+      } else {
+        cupB.push(cups[i]);
+      }
+    }
+    if (cupA.length > cupB.length) {
+      return givenCup
+    } else {
+      recCup = cupB[0];
+      return recCup;
+    }
+  }
+
+  //give recommended size
+  function giveRecSize(recBand, recCup) {
+    var size, cupIdx;
+    if (currentSize[0] > recBand) {
+      cupIdx = cupSizes.indexOf(recCup);
+      size = recBand + cupSizes[cupIdx + 1];
+      console.log(size);
+      return size;
+    } else if(currentSize[0] < recBand) {
+      cupIdx = cupSizes.indexOf(recCup);
+      size = recBand + cupSizes[cupIdx - 1];
+      console.log(size);
+      return size;
+    } else {
+      size = recBand + recCup;
+      console.log(size);
+      return size;
+    }
+  }
 
 })
