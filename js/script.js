a2249f2a63d2a23e59a762c63a94d542466dec45
KateStearns <katestearns@mac.com>
2015-09-21 17:03:14 -0700

    created fit survey

--- js/script.js
+++ js/script.js
@@ -8,51 +8,51 @@ $(document).ready(function() {
     return num;
   }
 
-  function getAnswer(num) {
-    $('#quest1').on('click', 'button', function(e) {
-      e.preventDefault();
-      var userAnswer = $('input:checked').val();
-      console.log(userAnswer);
-      getAnswer(grabQuestion(1));
-    })
-    $('#quest2').on('click', 'button', function(e) {
-      e.preventDefault();
-      var userAnswer = $('input:checked').val();
-      console.log(userAnswer);
-      getAnswer(grabQuestion(2));
-    })
-    $('#quest3').on('click', 'button', function(e) {
-      e.preventDefault();
-      var userAnswer = $('input:checked').val();
-      console.log(userAnswer);
-      getAnswer(grabQuestion(3));
-    })
-    $('#quest4').on('click', 'button', function(e) {
-      e.preventDefault();
-      var userAnswer = $('input:checked').val();
-      console.log(userAnswer);
-      getAnswer(grabQuestion(4));
-    })
-    $('#quest5').on('click', 'button', function(e) {
-      e.preventDefault();
-      var userAnswer = $('input:checked').val();
-      console.log(userAnswer);
-      getAnswer(grabQuestion(5));
-    })
-    $('#quest6').on('click', 'button', function(e) {
-      e.preventDefault();
-      var userAnswer = $('input:checked').val();
-      console.log(userAnswer);
-      getAnswer(grabQuestion(6));
-    })
-    $('#quest7').on('click', 'button', function(e) {
-      e.preventDefault();
-      var userAnswer = $('input:checked').val();
-      console.log(userAnswer);
-      getAnswer(grabQuestion(7));
-    })
-  }
-  getAnswer(grabQuestion(0));
+  $('#quest1').on('click', 'button', function(e) {
+    e.preventDefault();
+    var bandSize = $('#bandSize option:selected').val();
+    var cupSize = $('#cupSize option:selected').val();
+    console.log(bandSize, cupSize);
+    grabQuestion(1);
+  })
+  $('#quest2').on('click', 'button', function(e) {
+    e.preventDefault();
+    var question2Ans = $('#quest2 option:selected').val();
+    console.log(question2Ans);
+    grabQuestion(2);
+  })
+  $('#quest3').on('click', 'button', function(e) {
+    e.preventDefault();
+    var question3Ans = $('#question3 input:checked').val();
+    console.log(question3Ans);
+    grabQuestion(3);
+  })
+  $('#quest4').on('click', 'button', function(e) {
+    e.preventDefault();
+    var question4Ans = $('#question4 input:checked').val();
+    console.log(question4Ans);
+    grabQuestion(4);
+  })
+  $('#quest5').on('click', 'button', function(e) {
+    e.preventDefault();
+    var question5Ans = $('#question5 input:checked').val();
+    console.log(question5Ans);
+    grabQuestion(5);
+  })
+  $('#quest6').on('click', 'button', function(e) {
+    e.preventDefault();
+    var question6Ans = $('#question6 input:checked').val();
+    console.log(question6Ans);
+    grabQuestion(6);
+  })
+  $('#quest7').on('click', 'button', function(e) {
+    e.preventDefault();
+    var question7Ans = $('input:checked').val();
+    console.log(question7Ans);
+    grabQuestion(7);
+  })
+
+  grabQuestion(0);
 
 
 
