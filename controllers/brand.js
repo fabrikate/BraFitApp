eff95df133f9164e5eb521a9053bc6886b0b1f49
KateStearns <katestearns@mac.com>
2015-09-27 18:36:57 -0700

    add brands model, populating bug

--- controllers/brand.js
+++ controllers/brand.js
@@ -2,14 +2,18 @@ db = require('../models/index');
 
 // add styles to brand database
 app.get('/brandInfo', function (req, res) {
-  res.render('brand/index')
+  db.Brand.find({}, function(err, brands) {
+    if(err) {
+      console.log('Error is: ', err);
+    } else {
+      console.log(brands);
+      res.render('brand/index', {brand: brands})
+
+    }
+  })
 })
 
-app.post('/brandInfo', function (req, res) {
-  var newBrands = req.body;
-  console.log('new brands is: ', newBrands);
-  db.Brand.create({brandName:req.body.brand.brandName, popularStyles:req.body.brand.popularStyles});
-})
+
 // SEARCH VIA CUSTOM GOOGLE SEARCH
 var customSearchId = "AIzaSyAkuDc9QjydzXhntI0U4wdtgHk14t6dH7E&cx="
 var url = "https://www.googleapis.com/customsearch/v1?key=" + customSearchId + "011253441330498667417:mrhvtbzrjb8&q="
