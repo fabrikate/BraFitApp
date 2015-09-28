a67f442f59c8d6b215582ff109594a56273ea9d3
KateStearns <katestearns@mac.com>
2015-09-28 13:31:23 -0700

    heroku error, ./wishlist model

--- models/index.js
+++ models/index.js
@@ -4,4 +4,4 @@ mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/braFit_app');
 module.exports.User = require('./user');
 module.exports.Size = require('./size');
 module.exports.Brand = require('./brand');
-module.exports.wishList = require('./wishList');
+module.exports.wishList = require('./wishlist');
